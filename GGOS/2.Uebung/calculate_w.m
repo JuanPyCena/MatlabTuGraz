f66f04e3756cf65c90313184c5073916f27913b1
JuanPyCena <felix.sonnleitner@gmx.at>
2018-01-14 18:47:37 +0100

    added functino calculatew.m and modified main.m

---
+++ GGOS/2.Uebung/calculate_w.m
@@ -0,0 +1,76 @@
+function result = calculate_w(x, w_in, T_g0, h, r_sun, r_moon, dh, coefficient_F, coefficient_T_r, delta_x)
+    c_20 = x(1);
+    c_21 = x(2);
+    c_22 = x(3);
+    s_21 = x(4);
+    s_22 = x(5);
+    k_re = x(6);
+    k_im = x(7);
+    A    = x(8);
+    B    = x(9);
+    C    = x(10);
+
+    delta_c20 = delta_x(1);
+    delta_c21 = delta_x(2);
+    delta_c22 = delta_x(3);
+    delta_s21 = delta_x(4);
+    delta_s22 = delta_x(5);
+    delta_kre = delta_x(6);
+    delta_kim = delta_x(7);
+    delta_A   = delta_x(8);
+    delta_B   = delta_x(9);
+    delta_C   = delta_x(10);
+    
+    c_20 = c_20 + delta_c20;
+    c_21 = c_21 + delta_c21;
+    c_22 = c_22 + delta_c22;
+    s_21 = s_21 + delta_s21;
+    s_22 = s_22 + delta_s22;
+    k_re = k_re + delta_kre;
+    k_im = k_im + delta_kim;
+    A    = A    + delta_A;
+    B    = B    + delta_B;
+    C    = C    + delta_C;
+    
+    w_x = w_in(1);
+    w_y = w_in(2);
+    w_z = w_in(3);
+    
+    dist_sun   = sqrt(r_sun(1).^2 + r_sun(2).^2 + r_sun(3).^2);
+    dist_moon  = sqrt(r_moon(1).^2 + r_moon(2).^2 + r_moon(3).^2);
+
+    M_sun(1,:) = (((C-B) .* r_sun(2) .* r_sun(3)) ./ (dist_sun.^5)) .* (3 * GM_sun);
+    M_sun(2,:) = (((A-C) .* r_sun(1) .* r_sun(3)) ./ (dist_sun.^5)) .* (3 * GM_sun);
+    M_sun(3,:) = (((B-A) .* r_sun(1) .* r_sun(2)) ./ (dist_sun.^5)) .* (3 * GM_sun);
+
+    M_moon(1,:) = (((C-B) .* r_moon(2) .* r_moon(3)) ./ (dist_moon.^5)) .* (3 * GM_moon);
+    M_moon(2,:) = (((A-C) .* r_moon(1) .* r_moon(3)) ./ (dist_moon.^5)) .* (3 * GM_moon);
+    M_moon(3,:) = (((B-A) .* r_moon(1) .* r_moon(2)) ./ (dist_moon.^5)) .* (3 * GM_moon);
+
+    M = M_sun + M_moon;
+    
+    T_g_c1 = coefficient_T_g .* [((1/sqrt(3)) .* c_20 - c_22); -s_22; -c_21];
+    T_g_c2 = coefficient_T_g .* [-s_22; ((1/sqrt(3)) .* c_20 + c_22); -s_21];
+    T_g_c3 = coefficient_T_g .* [-c_21; -s_21; -(2/sqrt(3)) .* c_20];
+    
+    T_g_c1 = T_g_c1(1) + tr/3;
+    T_g_c2 = T_g_c2(2) + tr/3;
+    T_g_c3 = T_g_c3(3) + tr/3;
+    
+    T_g = [T_g_c1, T_g_c2, T_g_c3];
+    
+    dT_g = T_g - T_g0;
+    
+    T_r = coefficient_T_r .* [0,     0,      k_re * w_x + k_im * w_y;
+                              0,     0,      k_re * w_y - k_im * w_x;
+            k_re * w_x + k_im * w_y,     k_re * w_y - k_im * w_x,         0];
+        
+    F = T_g + T_r + coefficient_F .* [k_re, k_im, 0;
+                                     -k_im, k_re, 0;
+                                     0   ,  0  , 0];
+    
+    w = w_in + dt .* (F\(M - (dT_g * w_in) - cross(w_in, (T_g + T_r) * w_in) - cross(w_in,h) - dh)); 
+    
+    result = [T_g,w];
+end
+
--- GGOS/2.Uebung/main.m
+++ GGOS/2.Uebung/main.m
@@ -1,24 +1,21 @@
 clear all;
 close all;
 clc
-%% Feeding in data
+%% General Values
+
 AAM_FILE = 'ESMGFZ_AAM_v1.0_03h_2004.asc';
 HAM_FILE = 'ESMGFZ_HAM_v1.2_24h_2004.asc';
 OAM_FILE = 'ESMGFZ_OAM_v1.0_03h_2004.asc';
 
 % To get another set of data for the calculation change the file names. The
 % sample rate will be adapted automatically
-[timespan, step, h, grav_potent,r_moon,r_sun, reference] = read_data(OAM_FILE);
-r_moon = r_moon;
-r_sun = r_sun;
-initial = reference(:,1);
-c_20 = grav_potent(1, :);
-c_21 = grav_potent(2, :);
-s_21 = grav_potent(3, :);
-c_22 = grav_potent(4, :);
-s_22 = grav_potent(5, :);
-
-%% General Values
+[timespan, step, h, grav_potent,r_moon,r_sun, reference] = read_data(HAM_FILE);
+w_initial = reference(:,1);
+c_20_initial = grav_potent(1, 1);
+c_21_initial = grav_potent(2, 1);
+s_21_initial = grav_potent(3, 1);
+c_22_initial = grav_potent(4, 1);
+s_22_initial = grav_potent(5, 1);
 
 G       = (6.674e-11) * (3600 * 3600);        % [m^3/(kg* h^2)]
 GM_sun  = (1.32712442076e20) * (3600 * 3600); % [m^3/ h^2]
@@ -37,119 +34,33 @@ coefficient_T_g = sqrt(5/3) * Mass * R * R;
 coefficient_T_r = (omega_N * R^5) / (3 * G);
 coefficient_F   = (omega_N * omega_N * R^5) / (3 * G);
 
-%%
-dist_sun   = sqrt(r_sun(1,:).^2 + r_sun(2,:).^2 + r_sun(3,:).^2);
-dist_moon   = sqrt(r_moon(1,:).^2 + r_moon(2,:).^2 + r_moon(3,:).^2);
-
-M_sun(1,:) = (((C-B) .* r_sun(2,:) .* r_sun(3,:)) ./ (dist_sun.^5)) .* (3 * GM_sun);
-M_sun(2,:) = (((A-C) .* r_sun(1,:) .* r_sun(3,:)) ./ (dist_sun.^5)) .* (3 * GM_sun);
-M_sun(3,:) = (((B-A) .* r_sun(1,:) .* r_sun(2,:)) ./ (dist_sun.^5)) .* (3 * GM_sun);
-
-M_moon(1,:) = (((C-B) .* r_moon(2,:) .* r_moon(3,:)) ./ (dist_moon.^5)) .* (3 * GM_moon);
-M_moon(2,:) = (((A-C) .* r_moon(1,:) .* r_moon(3,:)) ./ (dist_moon.^5)) .* (3 * GM_moon);
-M_moon(3,:) = (((B-A) .* r_moon(1,:) .* r_moon(2,:)) ./ (dist_moon.^5)) .* (3 * GM_moon);
-
-M = M_sun + M_moon;
-
-%% 
-T_g_c1 = coefficient_T_g .* [((1/sqrt(3)) .* c_20 - c_22); -s_22; -c_21];
-T_g_c2 = coefficient_T_g .* [-s_22; ((1/sqrt(3)) .* c_20 + c_22); -s_21];
-T_g_c3 = coefficient_T_g .* [-c_21; -s_21; -(2/sqrt(3)) .* c_20];
-
-T_g_c1(1,:) = T_g_c1(1,:) + tr/3;
-T_g_c2(2,:) = T_g_c2(2,:) + tr/3;
-T_g_c3(3,:) = T_g_c3(3,:) + tr/3;
-
-T_g_c1(:,end+1) = T_g_c1(:,end);
-T_g_c2(:,end+1) = T_g_c2(:,end);
-T_g_c3(:,end+1) = T_g_c3(:,end);
-
-
-h(:,end+1) = h(:,end);
-
-for time = 1:timespan+1
-    T_g{time}  = [T_g_c1(:, time),T_g_c2(:, time), T_g_c3(:, time)];
-end
-
-for time = 1:timespan
-    dT_g{time} = T_g{time + 1} - T_g{time};
-end
-
-for time = 1:(length(h)-1)
-    dh{time} = h(:,time + 1) - h(:,time);
-end
-
 
 %% Calculation
 dt = 1;
 counter = 1;
-w = initial.*3600;
+w_in = w_initial.*3600;
+
+
+x          = [c_20_initial, c_21_initial, c_22_initial, s_21_initial, s_22_initial, k_re, k_im, A,  B, C]';
+delta_x    = [1,1,1,1,1,1,1,1,1,1]';
+zero_delta = zeros(10,1)
+
+A = zeros(3,10);
+
+T_g = coefficient_T_g .* [((1/sqrt(3)) .* c_20_initial - c_22_initial) + tr/3,        -s_22_initial,           -c_21_inital
+                           -s_22_initial,              ((1/sqrt(3)) .* c_20_initial + c_22_initial) + tr/3,   -s_21_initial
+                          -c_21_inital,                    -s_21_inital,                -(2/sqrt(3)) .* c_20_inital + tr/3];
+                      
 for t = 1:dt:24*365
-    
-    w_x = w(1);
-    w_y = w(2);
-    w_z = w(3);
-    
-    T_r = coefficient_T_r .* [0,     0,      k_re * w_x + k_im * w_y;
-                              0,     0,      k_re * w_y - k_im * w_x;
-            k_re * w_x + k_im * w_y,     k_re * w_y - k_im * w_x,         0];
-        
-    disp ('Tag: ')
-    disp (t/24) 
-    F = T_g{t} + T_r + coefficient_F .* [k_re, k_im, 0;
-                                        -k_im, k_re, 0;
-                                         0   ,  0  , 0];
-    
-    w = w + dt .* (F\(M(:,t) - (dT_g{t} * w) - cross(w, (T_g{t} + T_r) * w) - cross(w,h(:,t)) - dh{t})); 
-    
-    result{t} = w;
+   [T_g_plus_delta_x, w_plus_delta_x] = calculate_w(x, w_in, T_g, h, r_sun, r_moon, dh, coefficient_F, coefficient_T_r, delta_x);
+   [T_g, w_0]                         = calculate_w(x, w_in, T_g, h, r_sun, r_moon, dh, coefficient_F, coefficient_T_r, zero_delta);
+   
+   %Elements for A
+   
+   
 end
-%%
+
 result_m = cell2mat(result);
-
-xp        = (R/omega_N) .* result_m(1,:);
-yp        = (R/omega_N) .* result_m(2,:);
-
-xp_reference = (R/omega_N) .* reference(1,:) .* 3600;
-yp_reference = (R/omega_N) .* reference(2,:) .* 3600;
-
-delta_LOD = 86400/omega_N .* (omega_N - result_m(3,:)./3600);
-LOD = 8.6376e+04 - delta_LOD;
-LOD = LOD .* 1000;
-figure(1)
-plot(xp,yp)
-title('Polar Motion at Earth Surface')
-ylabel('y[m]')
-xlabel('x[m]')
-
-figure(2)
-plot(delta_LOD)
-title('Length of day')
-ylabel('Length of Day [s]')
-xlabel('Time [h]')
-
-figure(3)
-subplot(3,1,1)
-plot(result_m(1,:)./3600)
-title('Angular velocity - w_x')
-ylabel('w_x [rad/s]')
-xlabel('Time [h]')
-subplot(3,1,2)
-plot(result_m(2,:)./3600)
-title('Angular velocity - w_y')
-ylabel('w_y [rad/s]')
-xlabel('Time [h]')
-subplot(3,1,3)
-plot(result_m(3,:)./3600)
-title('Angular velocity - w_z')
-ylabel('w_z [rad/s]')
-xlabel('Time [h]')
-
-figure(4)
-plot(LOD)
-title('Delta Length of day')
-ylabel('Length of Day [ms]')
-xlabel('Time [h]')
 
 
 
