7e80e7ebb781b8dcc366ad90cf463e633dabb6a0
JuanPyCena <felix.sonnleitner@gmx.at>
2018-01-16 13:15:54 +0100

    made a loop for all phi

--- Methods of Modelling and Simulation/KirchengastÜbung/main.m
+++ Methods of Modelling and Simulation/KirchengastÜbung/main.m
@@ -16,10 +16,15 @@ begin = r0;
 stop  = Re + z;
 step  = 0.1;
 
-[theta_euler, r_euler] = f_euler(phi0, Hn, N0, theta0, begin, step, stop);
-[theta_rk, r_rk]       = f_rungeKutta(phi0, Hn, N0, theta0, begin, step, stop);
+for i = 1:4
 
+    [theta_euler(i,:), r_euler(i,:)] = f_euler(phi0(i), Hn, N0, theta0, begin, step, stop);
+    [theta_rk(i,:), r_rk(i,:)]       = f_rungeKutta(phi0(i), Hn, N0, theta0, begin, step, stop);
+
+end
+%%
+line = 2;
 hold on 
-plot(r_euler,theta_euler)
-plot(r_rk,theta_rk)
+plot(r_euler(line,:),theta_euler(line,:))
+plot(r_rk(line,:),theta_rk(line,:))
 hold off
