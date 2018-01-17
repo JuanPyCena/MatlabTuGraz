4a4d98ea32883534e89af984ea3b97206bb2a7de
JuanPyCena <felix.sonnleitner@gmx.at>
2018-01-17 16:22:07 +0100

    corrected errors

--- Methods of Modelling and Simulation/KirchengastÜbung/main.m
+++ Methods of Modelling and Simulation/KirchengastÜbung/main.m
@@ -6,8 +6,8 @@ close all;
 
 Re     = 6371;           %km
 z      = 650;            %km
-theta0 = 0;              %Grad
-phi0   = [0 30 60 89.9]; %Grad
+theta0 = 0;              %rad
+phi0   = [0 30 60 89.9] .* pi/180; %rad
 N0     = 315;            %N-units
 Hn     = 7;              %km
 r0     = Re;             %km
@@ -23,6 +23,7 @@ errorPlot            = true;
 
 diary('Output.txt')
 diary on
+disp('-----------------------------START--------------------------')
 for n = 1:numel(h)
     step  = h(n);
     
@@ -31,10 +32,11 @@ for n = 1:numel(h)
         [theta_euler(i,:), r_euler(i,:)] = f_euler(phi0(i), Hn, N0, theta0, begin, step, stop);
         [theta_rk(i,:), r_rk(i,:)]       = f_rungeKutta(phi0(i), Hn, N0, theta0, begin, step, stop);
         
-        euler_curve(i,n) = theta_euler(i,end) .* Re .* (pi/180);
-        rk_curve(i,n)    = theta_rk(i,end) .* Re .* (pi/180);
-        disp(['horizontal distance d for euler beams = ',num2str(euler_curve(i,n)),' km for h = ', num2str(step)])
-        disp(['horizontal distance d for runge-kutta beams = ',num2str(rk_curve(i,n)),' km for h = ', num2str(step)])
+        euler_curve(i,n) = theta_euler(i,end) * Re;
+        rk_curve(i,n)    = theta_rk(i,end) * Re;
+        disp(['Angle phi = ', num2str(phi0(i))])
+        disp(['horizontal distance d for euler beams = ', num2str(euler_curve(i,n)),' km for h = ', num2str(step)])
+        disp(['horizontal distance d for runge-kutta beams = ', num2str(rk_curve(i,n)),' km for h = ', num2str(step)])
     end
 
     diff = abs((theta_rk - theta_euler)./theta_rk);
@@ -47,18 +49,22 @@ for n = 1:numel(h)
         polarplot(circle,Re.*ones(size(circle))-Re, 'o')
         title(['Polarplot: h = ',num2str(step)])
         ax = gca;
+        ax.ThetaAxisUnits = 'degrees';
         ax.ThetaLim = [-30 30];
         ax.ThetaDir = 'clockwise';
         ax.ThetaZeroLocation = 'top';
         hold on
         % polarplot for each phi
         for line = 1:numel(phi0)
-            polarplot(theta_euler(line,:), r_euler(line,:).'-Re, '--')
-            polarplot(theta_rk(line,:), r_rk(line,:).'-Re)
+            polarplot(theta_euler(line,:), r_euler(line,:)-Re, '--')
+            polarplot(theta_rk(line,:), r_rk(line,:)-Re)
         end
         % Satelliteorbit plot
         polarplot(circle,z.*ones(size(circle)))
-        rlim([r_euler(1)-300-Re  r_euler(end)-Re]);
+        rlim([r_euler(1)-300-Re  r_euler(end)-Re+50]);
+        rticks([r_euler(1)-300-Re 0 r_euler(end)-Re])
+        thetaticks(-30:10:30)
+        rticklabels({['r = ',num2str(r_euler(1)-300), ' km'], ['r = ', num2str(Re), ' km'],['r = ',num2str(r_euler(end)), ' km']})
         hold off
         legendstring = {'R_E = r_0', ...
                         '\phi_{0,E} = 0','\phi_{0,RK} = 0',...
@@ -77,17 +83,21 @@ for n = 1:numel(h)
         hold on
         title(['Kartesischer Plot: h = ',num2str(step)])
         for line = 1:numel(phi0)
-            plot(r_euler(line,:), theta_euler(line,:), '--')
-            plot( r_rk(line,:), theta_rk(line,:))
+            d_euler = theta_euler(line,:) .* Re;
+            d_rk    = theta_rk(line,:) .* Re;
+            plot(r_euler(line,:), d_euler, '--')
+            plot(r_rk(line,:), d_rk)
         end
         hold off
+        xlabel('Radius [km]')
+        ylabel('Abweichung [km]')
         legendstring2 = {
                         '\phi_{0,E} = 0','\phi_{0,RK} = 0',...
                         '\phi_{0,E} = 30','\phi_{0,RK} = 30',...
                         '\phi_{0,E} = 60','\phi_{0,RK} = 60',...
                         '\phi_{0,E} = 89.9','\phi_{0,RK} = 89.9'
                         };
-        legend(legendstring2)
+        legend(legendstring2, 'location', 'northwest')
         print(['Kartesisch_', num2str(n)],'-dpdf')
    end
    
@@ -97,9 +107,13 @@ for n = 1:numel(h)
         hold on
         title(['Kartesischer Plot fuer die ersten 30 km: h = ',num2str(step)])
         for line = 1:numel(phi0)
-            plot(r_euler(line,1:30/step),theta_euler(line,1:30/step), '--')
-            plot(r_rk(line,1:30/step), theta_rk(line,1:30/step))
+            d_euler = theta_euler(line,1:30/step) .* Re;
+            d_rk    = theta_rk(line,1:30/step) .* Re;
+            plot(r_euler(line,1:30/step), d_euler, '--')
+            plot(r_rk(line,1:30/step), d_rk)
         end
+        xlabel('Radius [km]')
+        ylabel('Abweichung [km]')
         hold off
         legendstring2 = {
                         '\phi_{0,E} = 0','\phi_{0,RK} = 0',...
@@ -119,6 +133,8 @@ for n = 1:numel(h)
         for line = 1:numel(phi0)
             plot(r_euler(line,1:30/step), diff(line,1:30/step))
         end
+        xlabel('Radius [km]')
+        ylabel('Relativer Fehler [km/km]')
         hold off
         legendstring3 = {
                         '\phi_{0} = 0',...
@@ -138,4 +154,6 @@ for n = 1:numel(h)
         r_rk        = [];
     end
 end
+disp('----------------------------STOP--------------------------')
+disp('')
 diary off
